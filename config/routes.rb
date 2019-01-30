8124665e4b72bf6139bbf854ec0ab4f84f969772
mostafa <mostafanabil198@gmail.com>
2019-01-30 02:58:18 +0200

    add new action

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
 
+  resources :articles
+  # get 'articles', to: 'articles#show'
+  # get 'articles/new', to: 'articles#new'
+  # get 'articles/edit', to: 'articles#edit'
+  # get 'articles/delete', to: 'articles#delete'
+
 end
