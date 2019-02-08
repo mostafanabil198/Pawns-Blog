c03121426359e44cd3b01e1bd51b6c3981a0eb1b
mostafa <mostafanabil198@gmail.com>
2019-02-08 07:14:13 +0200

    add association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :articles
+  before_save { self.email = email.downcase }
   validates :userName, presence: true, length: {minumum: 3, maximum: 25}, uniqueness: {case_sensitive: false}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: {case_sesitive: false}, format: {with: VALID_EMAIL_REGEX}
