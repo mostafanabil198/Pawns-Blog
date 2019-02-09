24d5989622a31903b7c662f6f49ec00c21841194
mostafa <mostafanabil198@gmail.com>
2019-02-09 04:01:47 +0200

    add passwords to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   validates :userName, presence: true, length: {minumum: 3, maximum: 25}, uniqueness: {case_sensitive: false}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: {case_sesitive: false}, format: {with: VALID_EMAIL_REGEX}
+  has_secure_password
 end
