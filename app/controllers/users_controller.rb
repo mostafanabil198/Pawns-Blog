8c81543596e3e20e272adc99067c5f8660bbe27a
mostafa <mostafanabil198@gmail.com>
2019-02-11 01:16:07 +0200

    add user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,9 +31,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def index
+    @users = User.all
+  end
+
   private
   def user_params
     params.require(:user).permit(:userName, :email, :password )
   end
+
+
 
 end
